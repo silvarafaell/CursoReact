25915e95c2570ed02bb0f52109442ca7f390bb44
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-09-16 20:50:39 -0300

    Menu de navegacao

--- todo/src/components/App.tsx
+++ todo/src/components/App.tsx
@@ -4,8 +4,9 @@ import Navbar from "./Navbar";
 const App = () => {
     return (
         <div>
-            <h1>Meu Primeiro App</h1>
             <Navbar></Navbar>
+            <h1>Meu Primeiro App</h1>
+
         </div>
 
     );
